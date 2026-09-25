edeb9e11fb87650a12502b67df9eac25741a16b4
Abdullahi Aadam Robow <abdirobow66@gmail.com>
2026-09-25 09:02:01 +0300

    Add files via upload

---
+++ assigment/question1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <?php
+   $a = 5;
+$b = 6;
+$c = 8;
+
+if ($a >= $b && $a >= $c) {
+    $greatest = $a;
+} elseif ($b >= $a && $b >= $c) {
+    $greatest = $b;
+} else {
+    $greatest = $c;
+}
+
+if ($a <= $b && $a <= $c) {
+    $smallest = $a;
+} elseif ($b <= $a && $b <= $c) {
+    $smallest = $b;
+} else {
+    $smallest = $c;
+}
+
+echo "Greatest = " . $greatest . "<br>";
+echo "Smallest = " . $smallest;
+
+?>
+   ?> 
+</body>
+</html>
